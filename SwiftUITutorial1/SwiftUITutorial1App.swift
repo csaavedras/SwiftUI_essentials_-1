364387de77e55058e74ff73dc96fdee23ecb91b4
Camilo Saavedra <ci.saavedra@gmail.com>
2024-04-21 12:42:45 -0400

    Initial Commit

---
+++ SwiftUITutorial1/SwiftUITutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorial1App.swift
+//  SwiftUITutorial1
+//
+//  Created by Camilo Saavedra Salas on 21-04-24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
